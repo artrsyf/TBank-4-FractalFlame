b796bc06752a97708ba497553da09b8fc28c6ba1
artrsyf <rom.fadeev2017@yandex.ru>
2024-12-08 20:41:53 +0300

    Add enum for symmetry functionality

---
+++ src/main/scala/fractalflame/shared/eums/SymmetryType/SymmetryType.scala
@@ -0,0 +1,9 @@
+package fractalflame.shared.eums.SymmetryType
+
+enum SymmetryType:
+    case NoneSymmetry
+    case ReflectX
+    case ReflectY
+    case ReflectCenter
+    case Rotate90
+    case Rotate180
